23d1c32ccbe8001b969c5e6ed08d22a24aff7b04
Hillel1408 <etemax@bk.ru>
2024-05-31 13:01:46 +0300

    registration

--- #src/js/script.js
+++ #src/js/script.js
@@ -9,3 +9,15 @@ const entry = document.querySelector(".header__entry");
 openEntry.addEventListener("click", () => {
   entry.classList.toggle("active");
 });
+
+const openRegistration = document.querySelector(".open-registration");
+const registration = document.querySelector(".header__registration");
+openRegistration.addEventListener("click", () => {
+  entry.classList.toggle("active");
+  registration.classList.toggle("active");
+});
+
+const registrationBtn = document.querySelector(".registration-btn");
+registrationBtn.addEventListener("click", () => {
+  registration.classList.toggle("active");
+});
