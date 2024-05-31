3943ba037c61aeef4df27f285e7b236108a945ed
Hillel1408 <etemax@bk.ru>
2024-05-31 12:30:02 +0300

    entry

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,3 +3,9 @@ const menu = document.querySelector(".header__menu");
 openMenu.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+const openEntry = document.querySelector(".open-entry");
+const entry = document.querySelector(".header__entry");
+openEntry.addEventListener("click", () => {
+  entry.classList.toggle("active");
+});
